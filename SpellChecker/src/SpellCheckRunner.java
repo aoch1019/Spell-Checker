974f41c60359fa0acc45a1446ff44eedd76c3a63
andrewoch <andrewoch@10.0.0.193>
2020-11-03 14:37:41 -0500

    Added a SpellCheckRunner

---
+++ SpellChecker/src/SpellCheckRunner.java
@@ -0,0 +1,10 @@
+
+public class SpellCheckRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WordRecommenderUI test = new WordRecommenderUI();
+		test.ui();
+	}
+
+}

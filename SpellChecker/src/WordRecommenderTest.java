5d744f9ab7513be5f056417e8c48b9e5910df933
andrewoch <andrewoch@10.0.0.193>
2020-11-03 13:54:36 -0500

    Tests the getSimilarity method

--- SpellChecker/src/WordRecommenderTest.java
+++ SpellChecker/src/WordRecommenderTest.java
@@ -5,13 +5,18 @@ import org.junit.jupiter.api.Test;
 
 class WordRecommenderTest {
 
-	@BeforeEach
-	void setUp() throws Exception {
-	}
-
 	@Test
-	void testMain() {
-		fail("Not yet implemented");
+	void testGetSimilarity() {
+		WordRecommender w1 = new WordRecommender("engDictionary.txt");
+		double sim1 = w1.getSimilarity("oblige", "oblivion");
+		assertEquals(sim1, 2.5);
+		double sim2 = w1.getSimilarity("aghast", "gross");
+		assertEquals(sim2, 1.5);
+		double sim3 = w1.getSimilarity("aghast", "hello");
+		assertEquals(sim3, 0.0);
+		double sim4 = w1.getSimilarity("", "hello");
+		assertEquals(sim4, 0.0);
+		double blank = w1.getSimilarity("", "");
+		assertEquals(blank, 0.0);
 	}
-
 }

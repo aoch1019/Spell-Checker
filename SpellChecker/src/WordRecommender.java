13250ed1335cf97dda838ae4a46f95a628212908
andrewoch <andrewoch@10.0.0.193>
2020-11-03 13:57:50 -0500

    Implements the getWordSuggestions method with a helper method

--- SpellChecker/src/WordRecommender.java
+++ SpellChecker/src/WordRecommender.java
@@ -45,6 +45,68 @@ public class WordRecommender {
 		return similar;
 	}
 	
+	public ArrayList<String> getWordSuggestions(String word, int tolerance, double commonPercent, int topN){
+		ArrayList<String> possibleWords = new ArrayList<String>();
+		
+		File f = new File(this.filename);
+		try {
+			Scanner fileScanner = new Scanner(f);
+			while (fileScanner.hasNextLine()) {
+				String currWord = fileScanner.nextLine();
+				if(currWord.length() <= word.length() + tolerance &&  
+				   currWord.length() >= word.length() - tolerance &&
+				   inCommon(currWord, word) >= commonPercent) {
+				   possibleWords.add(currWord);
+				}
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			System.out.println("maybe file name is incorrect");
+			e.printStackTrace();
+		}
+		
+		ArrayList<String> answer = new ArrayList<String>();
+		while(topN > 0 && possibleWords.size() > 0) {
+			double highSimilarity = 0.0;
+			String highWord = "";
+			for(String pos : possibleWords) {
+				double currSimilarity = getSimilarity(pos, word);
+				if(currSimilarity > highSimilarity) {
+					highWord = pos;
+					highSimilarity = currSimilarity;
+				}
+			}
+			answer.add(highWord);
+			possibleWords.remove(highWord);
+			topN--;
+		}
+
+		return answer;
+	}
+	
+	public double inCommon(String word1, String word2) {
+		Set<Character> s1 = new HashSet<Character>();
+		Set<Character> s2 = new HashSet<Character>();
+		
+		for(int i=0; i < word1.length(); i++) {
+			Character currChar = word1.charAt(i);
+			s1.add(currChar);
+		}
+		
+		for(int i=0; i < word2.length(); i++) {
+			Character currChar = word2.charAt(i);
+			s2.add(currChar);
+		}
+
+		int total = s1.size() + s2.size();
+		
+		s1.addAll(s2);
+		int denominator = s1.size();
+		int numerator = total - denominator;
+		
+		return (double) numerator / (double) denominator;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
